cc24e3ab1f9a16336d0f3d97b68c537c1e246d28
Anil Roy <anilroy.cce@gmail.com>
2024-11-26 23:58:12 +0530

    FAQ

--- script.js
+++ script.js
@@ -15,3 +15,24 @@ function typeGreeting(text, elementId, speed) {
   window.onload = function() {
     typeGreeting("All resources and tool's for learning and building!", "greeting", 30);
   };
+
+  const collapsibles = document.querySelectorAll('.collapsible');
+
+    // Loop through each button and add a click event listener
+    collapsibles.forEach(button => {
+        button.addEventListener('click', function () {
+            this.classList.toggle('active');
+            const content = this.nextElementSibling;
+
+            if (content.style.maxHeight) {
+                // Collapse the content
+                content.style.maxHeight = null;
+                content.classList.remove('open');
+            } else {
+                // Expand the content
+                content.style.maxHeight = content.scrollHeight + 'px';
+                content.classList.add('open');
+            }
+          
+        });
+    });
